bbf6860b73675538a4530c3e1bb3276167292037
VeNaGoD <evan.gobe.truong@gmail.com>
2022-06-02 19:54:28 +0200

    Make a transfer between an unusable branch (master) and new Sam branch

---
+++ Spéciale.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Speciales Speciales; 
+
+
+struct Speciales{
+    char nom[25];
+    float valeur;
+    char description[25];
+    int nb_tours_actifs;
+    int nb_tours_rechargement;
+    ff
+};
