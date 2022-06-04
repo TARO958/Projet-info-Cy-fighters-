37735350c4f045bfe3551dfca5be11c607ecee28
TARO958 <105594171+TARO958@users.noreply.github.com>
2022-06-05 01:32:23 +0200

    SPE 2

--- Speciale.c
+++ Speciale.c
@@ -10,10 +10,10 @@ Speciales tab[NB_ATT_SPE]={
     {.nom = "boule magique",.valeur = 1 , .description = "infligle des dégats sur un adversaire" ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
     {.nom = "boule de feu",.valeur =25,.description ="lance une boule de feu et brule l'énnemie pendart 2 tours"  ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
     {.nom = "lancer de shurikens",.valeur =25 ,.description = "lance un shurikens" ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
-    {.nom = "",.valeur = 25,.description = "géle l'adversaire" ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
-    {.nom = "decoupe",.valeur = 25 ,.description = "tranche" ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
+    {.nom = "soin miraculeux ",.valeur = 25,.description = "géle l'adversaire" ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
+    {.nom = "soin magique",.valeur = 25 ,.description = "tranche" ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
     {.nom = "protection",.valeur = 25,.description ="renforce la def du personnage de 35 % "   ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
-    {.nom = "tournis",.valeur =25,.description = "tourne rapidement sur soi-même avec l'épée en main " ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
+    {.nom = "tourbillon",.valeur =25,.description = "tourne rapidement sur soi-même avec l'épée en main " ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
     {.nom = "griffe",.valeur =25,.description =  "griffe l'adversaire",.nb_tours_actifs =1000 ,.nb_tours_rechargement},
     {.nom = "tempete",.valeur = 25 , .description ="lance une tempête sur l'équipe ennemie qui touche tout les personnages pendant 3 tours"  ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
     {.nom = "lance grenade",.valeur = 25,.description = "lance une grenade" ,.nb_tours_actifs = ,.nb_tours_rechargement},
@@ -26,10 +26,10 @@ Speciales tab[NB_ATT_SPE]={
     {.nom = "voile protecteur",.valeur = 25,.description ="augemente la def de toute l'quipe de 20% du personnage qui l'utilise"  ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
     {.nom = "fumigene",.valeur = 25,.description = "le personnage devient impossible a ciblé pendant 3 tours mais il ne peut pas attaquer au tour suivant "  ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
     {.nom = "mur de terre ",.valeur =25,.description = ""  ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
-    {.nom = "coup de coup percutant",.valeur =25,.description = "infligle des dégats colossaux sur un adversaire" ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
+    {.nom = "smash",.valeur =25,.description = "infligle des dégats  sur trois adversaires" ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
     {.nom = "",.valeur = 25, .description =  ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
     {.nom = "lance grenade",.valeur = 25,.description =  ,.nb_tours_actifs = ,.nb_tours_rechargement},
-    {.nom = "foudre",.valeur =25 ,.description =  ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
+    {.nom = "profonde entaille",.valeur =25 ,.description =  ,.nb_tours_actifs =1000 ,.nb_tours_rechargement},
 };
 
 
