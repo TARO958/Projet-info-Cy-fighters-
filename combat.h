f30586e449034b73c2be1f3521a427774a6cc7cb
VeNaGoD <evan.gobe.truong@gmail.com>
2022-06-04 21:20:37 +0200

    ca marche pas

--- combat.h
+++ combat.h
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include <stdlib.h>
-//void select_engagement(Personnages perso_select_joueur1[],Personnages perso_select_joueur2[]);
-//void select_perso();
 
+
+void trifusion();
 void tri_vit();
